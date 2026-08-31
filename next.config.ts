cbe0e5505cd2fc2c6548456d9f6012e58ff714db
sksayril <sksayril123@gmail.com>
2026-08-31 18:09:20 +0530

    fixed the isues

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,21 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "streaming-bucket-123.s3.us-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.us-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
+      },
     ],
   },
 };
 
 export default nextConfig;
+
